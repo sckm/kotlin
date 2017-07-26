23c2e63f62c74d69dbf13c82246fbbcc7aff8ff0
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-07-26 18:55:44 +0300

    ~

--- core/descriptors/src/org/jetbrains/kotlin/types/ErrorUtils.java
+++ core/descriptors/src/org/jetbrains/kotlin/types/ErrorUtils.java
@@ -213,6 +213,12 @@ public class ErrorUtils {
 
         @NotNull
         @Override
+        public Set<Name> getClassifierNames() {
+            return emptySet();
+        }
+
+        @NotNull
+        @Override
         public Collection<DeclarationDescriptor> getContributedDescriptors(
                 @NotNull DescriptorKindFilter kindFilter, @NotNull Function1<? super Name, Boolean> nameFilter
         ) {
@@ -281,6 +287,10 @@ public class ErrorUtils {
         @NotNull
         @Override
         public Set<Name> getVariableNames() {
+            throw new IllegalStateException();
+        }
+
+        public Set<Name> getClassifierNames() {
             throw new IllegalStateException();
         }
 
